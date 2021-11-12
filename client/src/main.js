22a074f6200a770d9edbbc7cf307cb0ef1cc5f71
Alec Tatton <alectatton1@gmail.com>
2021-11-11 18:40:39 -0700

    Styling and include vuetify

--- client/src/main.js
+++ client/src/main.js
@@ -1,10 +1,24 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import Vuetify from 'vuetify'
+import 'vuetify/dist/vuetify.min.css'
 
 Vue.config.productionTip = false
+
+Vue.use(Vuetify)
 
 new Vue({
   router,
   render: h => h(App)
 }).$mount('#app')
+
+
+/* eslint-disable no-new */
+// new Vue({
+//   el: '#app',
+//   router,
+//   template: '<App/>',
+//   components: { App }
+// })
+
